8652f8bff81f1818d7410366965308f55a6319b3
salohuddinov <salohuddinov@gmail.com>
2024-05-18 22:07:52 +0300

    muhammadal1

---
+++ src/components/product-wrapper/ProductWrapper.jsx
@@ -0,0 +1,53 @@
+"use client"
+import Image from 'next/image'
+import React from 'react'
+import './ProductWrapper.css'
+
+const ProductWrapper = ({ data }) => {
+    console.log(data)
+    let productItem = data?.products?.map(el => (
+        <div key={el.id} className="card">
+            <Image src={el.thumbnail} alt={el.title} width={200} height={200} />
+            <h2>{el.title}</h2>
+            <br />
+            <div className="price">
+                <h2>${el.price}</h2>
+                <h3>${el.discountPercentage}</h3>
+            </div>
+            <div className="star">
+                <div class="rating">
+                    <input type="radio" id="star5" name="rate" value="5" />
+                    <label for="star5" title="text"></label>
+                    <input type="radio" id="star4" name="rate" value="4" />
+                    <label for="star4" title="text"></label>
+                    <input type="radio" id="star3" name="rate" value="3" />
+                    <label for="star3" title="text"></label>
+                    <input type="radio" id="star2" name="rate" value="2" />
+                    <label for="star2" title="text"></label>
+                    <input checked="" type="radio" id="star1" name="rate" value="1" />
+                    <label for="star1" title="text"></label>
+                </div>
+                <h4>( {el.rating} )</h4>
+            </div>
+        </div>
+    ))
+    return (
+        <>
+            <div className="wrapper__title">
+                <h2>Каталог</h2>
+                <div className="catalog__buton">
+                    <button>SIMPLE</button>
+                    <button>SIMPLE ALCANTARA</button>
+                    <button>ADVANCED</button>
+                    <button>ADVANCED ALCANTARA</button>
+
+                </div>
+            </div>
+            <div className="wrapper">
+                {productItem}
+            </div>
+        </>
+    )
+}
+
+export default ProductWrapper
